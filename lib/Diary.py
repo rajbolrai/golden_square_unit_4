58086fb846b2ea4dde5639d2e72ba56f66ff4a72
rajbolrai <rajbolrai@hotmail.com>
2025-06-08 22:34:27 +0100

    testing done for single class

--- lib/Diary.py
+++ lib/Diary.py
@@ -6,8 +6,15 @@ class DiaryEntry:
         # Parameters:
         #   title: string
         #   contents: string
+        if type(title) != str or type(contents) != str:
+            raise TypeError("Error: invalid type. Expected String")
+        
         self._title = title
         self._contents = contents
+        self._content_words = []
+        self._has_read = False
+        self.curr_word_count = 0
+        
 
     def format(self):
         # Returns:
@@ -23,6 +30,7 @@ class DiaryEntry:
         # contents_only_words = [i.strip(string.punctuation) for i in self._contents.split() if i.strip(string.punctuation).isalpha()]
         title_words = re.findall(r"\b[a-zA-Z']+(?:-[a-zA-Z']+)*\b", self._title)
         content_words = re.findall(r"\b[a-zA-Z']+(?:-[a-zA-Z']+)*\b", self._contents)
+        self._content_words = content_words
         return len(title_words) + len(content_words)
 
     def reading_time(self, wpm):
@@ -32,7 +40,10 @@ class DiaryEntry:
         # Returns:
         #   int: an estimate of the reading time in minutes for the contents at
         #        the given wpm.
-        pass
+        
+        time_taken_to_read = round(self.count_words()/wpm,2)
+        return f"{time_taken_to_read} mins"
+        
 
     def reading_chunk(self, wpm, minutes):
         # Parameters
@@ -47,8 +58,23 @@ class DiaryEntry:
         # If called again, `reading_chunk` should return the next chunk,
         # skipping what has already been read, until the contents is fully read.
         # The next call after that should restart from the beginning.
-        pass
-
-first = DiaryEntry(123,123123)
-
-print(first.format())
+        string_to_return = ""
+        test = self.count_words()
+        word_count = len(self._content_words)
+        
+        print(self._content_words)
+        
+        if  self.curr_word_count < word_count:
+            for i in range(wpm * minutes):
+                if self.curr_word_count == word_count:
+                    break
+                string_to_return += self._content_words[self.curr_word_count] + " "
+                self.curr_word_count += 1
+                #print(string_to_return)
+        elif self.curr_word_count == word_count:
+            self.curr_word_count = 0
+            
+            
+        return string_to_return.strip()
+    
+    
--- tests/test_diary.py
+++ tests/test_diary.py
@@ -1,11 +1,15 @@
 from lib.Diary import * 
-
+import pytest
 
 """ 
 Check to see if the title and content is of type string
 return invalid type error
 """
-
+def test_type_of_tile_content_is_string():
+    with pytest.raises(TypeError) as e:
+        DiaryEntry(1,2)
+    error_message = str(e.value)
+    assert error_message == "Error: invalid type. Expected String"
 """
 Given a title and content
 format will return a string with the corrected format
@@ -21,10 +25,66 @@ format will return a string with the corrected format
 """
 def test_given_valid_entry_output_correct_word_count():
     diary_entry = DiaryEntry("Thursday", "I was trying my hardest to 4' it's go-go-go go-go to-do let's stay locked in.?")
-    words_to_read = diary_entry.count_words()
-    assert words_to_read == "1 min"
-    
     assert diary_entry.count_words() == 15
 
 """ Given their wpm 
 return the time taken to read the contents at the given wpm in minutes"""
+
+def test_given_valid_entry_output_correct_word_per_minute():
+    diary_entry = DiaryEntry("Thursday", 
+                                "one two three four five six seven eight nine ten "
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                
+                                + "one two three four five six seven eight nine ten "                                 
+                                + "one two three four five six seven eight nine ten "                                
+                                )
+    word_per_minute = diary_entry.reading_time(100)
+    assert word_per_minute == "2.01 mins"
+    
+    
+def test_can_read_one_chunk():
+    diary_entry = DiaryEntry("Thursday", """
+                            "one two three four five six seven eight nine ten 
+                            one two three four five six seven eight nine ten                                 
+                            one two three four five six seven eight nine ten                                 
+                            one two three four five six seven eight nine ten                                 
+                            one two three four five six seven eight nine ten                                 
+                            one two three four five six seven eight nine ten                                 
+                            one two three four five six seven eight nine ten                                 
+                            one two three four five six seven eight nine ten                                 
+                            one two three four five six seven eight nine ten                             
+                            one two three four five six seven eight nine ten """                      
+                            )
+    curr_reading_chunk = diary_entry.reading_chunk(5,5)
+    #print(diary_entry._content_words)
+    assert curr_reading_chunk == """one two three four five six seven eight nine ten one two three four five six seven eight nine ten one two three four five"""
+    
+    
+def test_can_read_multiple_chunks():
+    diary_entry = DiaryEntry("Thursday", """
+                            "Welcome to my world. You will be greeted by the 
+                            unexpected here and your mind will be challenged and expanded
+                            in ways that you never thought possible. That is if
+                            you are able to survive."""                      
+                            )
+    diary_entry.reading_chunk(5,2)
+    curr_reading_chunk2 = diary_entry.reading_chunk(5,3)
+    
+    
+    
+    assert curr_reading_chunk2 == """unexpected here and your mind will be challenged and expanded in ways that you never"""
